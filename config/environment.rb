2a80da0a1c16c628bb2b6afffa7238663a2fde0a
Emmanuel Addo-Yirenkyi <emmanuel.addo.yirenkyi@gmail.com>
2012-11-13 13:43:36 +0000

    Why is not uploading

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+
+ActiveSupport::Deprecation.silenced = true 
+
 # Initialize the rails application
 RailsTutorialApp::Application.initialize!
+
+
+
